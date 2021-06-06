71b40d5dc0f87c0f73edc61dbab077c20a080a2e
Matt Daley <mdaleyhk@gmail.com>
2021-06-06 15:41:14 +0100

    adds base store logic for fetching and updating pokemon

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,27 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    pokemon: []
   },
   mutations: {
+    UPDATE_POKEMON(state, pokemonArray) {
+      state.pokemon = pokemonArray;
+    }
   },
   actions: {
-  },
-  modules: {
+    updatePokemon({ commit }, pokemonArray) {
+      commit('UPDATE_POKEMON', pokemonArray);
+    },
+    fetchPokemon({ dispatch }, numberOfPokemon) {
+      fetch(`https://pokeapi.co/api/v2/pokemon?limit=${numberOfPokemon}`)
+        .then((response) => response.json())
+        .then((data) => {
+          dispatch('updatePokemon', data.results);
+        });
+    }
   }
-})
+});
